882f51b4a3abe668e3dfbcf17b7e48ceab124f6a
maximmoolidHjHP53 <maximoolid123@email.com>
2026-08-05 10:50:12 +0800

    Fix syntax error on line 229

--- server.js
+++ server.js
@@ -226,8 +226,6 @@ const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } 
         }
     });
 
-});
-
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT} with full endpoint route mapping.`);
